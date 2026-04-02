8bc7c2922e887d70343fe5149ac30d51ebe87e16
Andrew Sutiono <sutionoandrew@gmail.com>
2026-04-02 15:44:47 +0700

    added landing page

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -1,24 +1,40 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
-
-import { useColorScheme } from '@/hooks/use-color-scheme';
-
-export const unstable_settings = {
-  anchor: '(tabs)',
-};
+import { StatusBar, useColorScheme, View } from 'react-native';
+import { Colors } from '@/constants/theme';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
+    const insets = useSafeAreaInsets();
+    const colorScheme = useColorScheme() ?? 'light';
+    const theme = Colors[colorScheme as keyof typeof Colors];
 
-  return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
-  );
+    return (
+        /* Adding 'key' here forces a full refresh when switching modes */
+        <View style={{ flex: 1, backgroundColor: theme.background }}>
+            <StatusBar
+                // Core RN uses these specific strings:
+                barStyle={colorScheme === 'dark' ? 'light-content' : 'dark-content'}
+
+                // Android specific: Makes it blend with your background
+                backgroundColor="transparent"
+                translucent={true}
+
+                // iOS specific: Hidden or visible
+                hidden={false}
+            />
+            <Stack
+                initialRouteName='index'
+                screenOptions={{
+                    headerStyle: { backgroundColor: theme.navBackground },
+                    headerTintColor: theme.title,
+                    contentStyle: { backgroundColor: theme.background }, // Important for Android
+                    headerShadowVisible: false,
+                }}
+            >
+                <Stack.Screen name="index" options={{ title: "Home" }} />
+                <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+                <Stack.Screen name="(dashboard)" options={{ headerShown: false }} />
+            </Stack>
+        </View>
+    );
 }
