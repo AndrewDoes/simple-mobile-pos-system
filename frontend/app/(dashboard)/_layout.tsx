722491bf9b7e452d2ec7e94128ca4291b1f8fa59
Andrew Sutiono <sutionoandrew@gmail.com>
2026-04-06 17:58:49 +0700

    added tab-bar for navigation

--- frontend/app/(dashboard)/_layout.tsx
+++ frontend/app/(dashboard)/_layout.tsx
@@ -1,11 +1,59 @@
-import { Stack } from 'expo-router';
+import { Colors } from '@/constants/theme';
+import { Stack, Tabs } from 'expo-router';
+import { useColorScheme } from 'react-native';
+import { Package, ShoppingCart, User } from 'lucide-react-native';
 
 export default function DashboardLayout() {
+    const colorScheme = useColorScheme();
+    const theme = Colors[colorScheme as keyof typeof Colors] || Colors.light;
+
     return (
-        <Stack screenOptions={{ headerShown: false }}>
-            <Stack.Screen name="product" />
-            <Stack.Screen name="profile" />
-            <Stack.Screen name="transaction" />
-        </Stack>
+        <Tabs
+            screenOptions={{
+                headerShown: false,
+                tabBarStyle: {
+                    backgroundColor: theme.navBackground,
+                    paddingTop: 10,
+                    height: 90
+                },
+                tabBarActiveTintColor: theme.tabIconSelected,
+                tabBarInactiveTintColor: theme.tabIconDefault,
+            }}>
+
+
+            <Tabs.Screen name="product" options={{
+                title: "Products", tabBarIcon: ({ focused }) => {
+                    return (
+                        focused ?
+                            <Package color={theme.tabIconSelected} />
+                            :
+                            <Package color={theme.tabIconDefault} />
+                    )
+                }
+            }} />
+
+            <Tabs.Screen name="transaction" options={{
+                title: "Transaction", tabBarIcon: ({ focused }) => {
+                    return (
+                        focused ?
+                            <ShoppingCart color={theme.tabIconSelected} />
+                            :
+                            <ShoppingCart color={theme.tabIconDefault} />
+                    )
+                }
+            }} />
+
+            <Tabs.Screen name="profile" options={{
+                title: "Profile", tabBarIcon: ({ focused }) => {
+                    return (
+                        focused ?
+                            <User color={theme.tabIconSelected} />
+                            :
+                            <User color={theme.tabIconDefault} />
+                    )
+                }
+            }} />
+
+        </Tabs>
     );
 }
